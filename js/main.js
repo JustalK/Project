ca30e489e8e64616a43ce9c30408ac4bba5bbe33
Latsuj <justal.kevin@gmail.com>
2015-10-12 23:08:12 +0200

    [2] Adding some content, Changing the first button

--- js/main.js
+++ js/main.js
@@ -1,28 +1,38 @@
 $(document).ready(function() {
 	var speed = 100;
 	
-	$("#block1").mouseenter(function() {
+	$("#block1hide").hide();
+	$("#block1").click(function() {
 		$(".block").each(function() {
 			$(this).clearQueue().stop();
+			$(this).css("cursor","auto");
 		});
-		$("#block6").css("border","none");
-		$("#block6").css("margin","0px");
-		$("#block6").animate({height: "0%"},speed,function() {
-			$("#block5").css("border","none");
-			$("#block5").css("margin","0px");
-			$("#block5").animate({height: "0%"},speed,function(){
-				$("#block4").css("border","none");
-				$("#block4").css("margin","0px");
-				$("#block4").animate({height: "0%"},speed,function() {
-					$("#block3").css("border","none");
-					$("#block3").css("margin","0px");
-					$("#block3").animate({height: "0%"},speed,function() {
-						$("#block2").css("border","none");
-						$("#block2").css("margin","0px");
-						$("#block2").animate({height: "0%"},speed,function() {
-							$(".block").not("#block1").hide();
-							$("#block1").animate( {width: "100%"},speed);
-							$("#block1").animate( {height: "100%"},speed);
+		$("#block1").animate({opacity: '0.5'},150, function() {
+			$("#block1").animate({opacity: '1'},150, function() {
+				$("#block6").css("border","none");
+				$("#block6").css("margin","0px");
+				$("#block6").animate({height: "0%"},speed,function() {
+					$("#block5").css("border","none");
+					$("#block5").css("margin","0px");
+					$("#block5").animate({height: "0%"},speed,function(){
+						$("#block4").css("border","none");
+						$("#block4").css("margin","0px");
+						$("#block4").animate({height: "0%"},speed,function() {
+							$("#block3").css("border","none");
+							$("#block3").css("margin","0px");
+							$("#block3").animate({height: "0%"},speed,function() {
+								$("#block2").css("border","none");
+								$("#block2").css("margin","0px");
+								$("#block2").animate({height: "0%"},speed,function() {
+									$(".block").not("#block1").hide();
+									$("#block1").animate( {width: "100%"},speed,function(){
+									});
+									$("#block1").animate( {height: "100%"},speed,function(){
+		
+										$("#block1hide").show();
+									});
+								});
+							});
 						});
 					});
 				});
@@ -33,23 +43,26 @@ $(document).ready(function() {
 	$("#block1").mouseleave(function() {
 		$(".block").each(function() {
 			$(this).clearQueue().stop();
+			$(this).css("cursor","pointer");
 		});
+		$("#block1").css("opacity","1");
+		$("#block1hide").hide();
 		$("#block1").animate({height: "50%"},speed);
 		$("#block1").animate({width: "30%"},speed,function(){
 			$(".block").show();
-			$("#block2").css("border","1px solid #FFFFFF");
+			$("#block2").css("border","1px solid #000000");
 			$("#block2").css("margin","-1px");
 			$("#block2").animate({height: "25%"},speed,function() {
-				$("#block3").css("border","1px solid #FFFFFF");
+				$("#block3").css("border","1px solid #000000");
 				$("#block3").css("margin","-1px");
 				$("#block3").animate({height: "25%"},speed,function(){
-					$("#block4").css("border","1px solid #FFFFFF");
+					$("#block4").css("border","1px solid #000000");
 					$("#block4").css("margin","-1px");
 					$("#block4").animate({height: "50%"},speed,function() {
-						$("#block5").css("border","1px solid #FFFFFF");
+						$("#block5").css("border","1px solid #000000");
 						$("#block5").css("margin","-1px");
 						$("#block5").animate({height: "25%"},speed,function() {
-							$("#block6").css("border","1px solid #FFFFFF");
+							$("#block6").css("border","1px solid #000000");
 							$("#block6").css("margin","-1px");
 							$("#block6").animate({height: "25%"},speed);
 						});
